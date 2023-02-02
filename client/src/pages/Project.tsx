3b294685ace37c8ac9499d11a234f39932e549d9
JACE <jeysonm021@gmail.com>
2023-02-02 09:03:27 -0500

    test7

--- client/src/pages/Project.tsx
+++ client/src/pages/Project.tsx
@@ -16,6 +16,7 @@ export const Project = () => {
 
   return (
     <>
+      <p>HIHI</p>
       {!loading && !error && (
         <div className="mx-auto w-75 card p-5">
           <Link to="/" className="btn btn-light btn-sm w-25 d-inline ms-auto">
