a79999834929bba7852ebb9446e39ca34b8831da
JACE <jeysonm021@gmail.com>
2023-02-02 09:27:46 -0500

    testint test

--- client/src/Index.tsx
+++ client/src/Index.tsx
@@ -40,7 +40,7 @@ export default function Index() {
   // The useRoutes() hook allows you to define your routes as JavaScript objects
   // instead of <Routes> and <Route> elements. This is really just a style
   // preference for those who prefer to not use JSX for their routes config.
-  let element = useRoutes(routes2);
+  let element = useRoutes(routes);
 
   return (
     <div>
