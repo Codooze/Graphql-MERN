b899ceefe9d8cea9c56d49b1b0575a3a62115f3c
JACE <jeysonm021@gmail.com>
2023-02-02 09:55:52 -0500

    testestestestestes

--- client/src/Index.tsx
+++ client/src/Index.tsx
@@ -79,7 +79,7 @@ function Layout() {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/courses">Courses</Link>
+            <Link to="/project">Courses</Link>
           </li>
           <li>
             <Link to="/nothing-here">Nothing Here</Link>
