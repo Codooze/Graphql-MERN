f1c66195db86cd7a09f91b52949189588ca57e6a
JACE <jeysonm021@gmail.com>
2023-02-02 09:16:23 -0500

    test 10

--- client/src/Index.tsx
+++ client/src/Index.tsx
@@ -1,13 +1,24 @@
 import type { RouteObject } from "react-router-dom";
 import { Outlet, Link, useRoutes, useParams } from "react-router-dom";
-import Project from "./pages/Project";
 
 export default function Index() {
   let routes: RouteObject[] = [
-    { path: "/courses/:id", element: <Course /> },
-    { path: "/project/:id", element: <Project /> },
-
-    { path: "*", element: <NoMatch /> },
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { index: true, element: <Home /> },
+        {
+          path: "/courses",
+          element: <Courses />,
+          children: [
+            { index: true, element: <CoursesIndex /> },
+            { path: "/courses/:id", element: <Course /> },
+          ],
+        },
+        { path: "*", element: <NoMatch /> },
+      ],
+    },
   ];
 
   // The useRoutes() hook allows you to define your routes as JavaScript objects
@@ -17,7 +28,24 @@ export default function Index() {
 
   return (
     <div>
-      <h1>RoutExample</h1>
+      <h1>Route Objects Example</h1>
+
+      <p>
+        This example demonstrates how to use React Router's "route object" API
+        instead of the JSX API to configure your routes. Both APIs are
+        first-class. In fact, React Router actually uses the object-based API
+        internally by creating route objects from your{" "}
+        <code>&lt;Route&gt;</code>
+        elements.
+      </p>
+
+      <p>
+        React Router exposes a <code>useRoutes()</code> hook that allows you to
+        hook into the same matching algorithm that <code>&lt;Routes&gt;</code>{" "}
+        uses internally to decide which <code>&lt;Route&gt;</code> to render.
+        When you use this hook, you get back an element that will render your
+        entire route hierarchy.
+      </p>
 
       {element}
     </div>
@@ -44,6 +72,45 @@ function Layout() {
       <hr />
 
       <Outlet />
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function Courses() {
+  return (
+    <div>
+      <h2>Courses</h2>
+      <Outlet />
+    </div>
+  );
+}
+
+function CoursesIndex() {
+  return (
+    <div>
+      <p>Please choose a course:</p>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="react-fundamentals">React Fundamentals</Link>
+          </li>
+          <li>
+            <Link to="advanced-react">Advanced React</Link>
+          </li>
+          <li>
+            <Link to="react-router">React Router</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
